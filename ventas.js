c3dfc8cca7897647c319b12e90cb30ee420dd4d9
MartinAlejandro7 <alejomartin7532@gmail.com>
2026-07-02 11:36:41 -0500

    se probo el funcionamiento de VALIDACIONES

--- ventas.js
+++ ventas.js
@@ -10,8 +10,37 @@ function calcularComision(numeroVentas, precioProducto) {
     return comision
 }
 
+
+function validarInput(input, idError) {
+
+    let valor = input.value;
+    let error = document.getElementById(idError);
+
+    error.textContent = "";
+
+    if (valor == "") {
+        error.textContent = "Este campo no puede estar vacío.";
+        return false;
+    }
+
+    if (isNaN(valor)) {
+        error.textContent = "Solo se permiten números.";
+        return false;
+    }
+
+    if (valor.length > 5) {
+        error.textContent = "Máximo 5 dígitos.";
+        return false;
+    }
+
+    return true;
+}
+
+
 function calculadora() {
-    
+
+
+
     let sueldoBase = recuperarFloat("txtSueldoBase")
     let numeroVentas = recuperarFloat("txtVentas");
     let precioProducto = recuperarFloat("txtPrecio")
